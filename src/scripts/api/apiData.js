f42424891bfbcf84151054f1de315ec2e5197be4
maty <maty@maty>
2020-04-12 00:33:43 +0200

    created api connector for fetch list of all available pokemons and get data about specific pokemon

--- src/scripts/api/apiData.js
+++ src/scripts/api/apiData.js
@@ -1,10 +1,26 @@
 import axios from 'axios';
-
-export default { getPokemon };
+import regeneratorRuntime from "regenerator-runtime";
 
 async function getPokemon(pokemonName) {
-  await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then( response => {
-      console.log('response', response);
-    })
+  return await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then( response => response.data);
 }
+
+async function getAllPokemonNames() {
+  var allPokemons = [];
+  await axios.get(`https://pokeapi.co/api/v2/pokemon`)
+    .then( response => {
+      response.data.results.forEach( (element, index) => {
+        allPokemons.push(
+          {
+            id: index,
+            name: element.name
+          }
+        );
+      });
+    });
+
+  return allPokemons;
+}
+
+export default { getPokemon, getAllPokemonNames };
